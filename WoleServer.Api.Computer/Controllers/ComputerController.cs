e14ad3a6ae32f0bd78761e60056ccd2265228a94
Thibaut RENONCOURT <trenoncourt@access-it.fr>
2017-05-04 11:42:27 +0200

    Add organisation. Add computer controller empty.

---
+++ WoleServer.Api.Computer/Controllers/ComputerController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WoleServer.Api.Computer.Controllers
+{
+    /// <summary>
+    /// Rest endpoint for computers.
+    /// </summary>
+    [Route("api/computers")]
+    public class ComputerController
+    {
+    }
+}
